cb0019095596a3b044c98f39f172a0e7de578431
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2023-12-03 11:36:30 +0800

    Update pvz.cpp

--- pvz.cpp
+++ pvz.cpp
@@ -2,10 +2,52 @@
 #include<graphics.h>
 #include<cstring>
 using namespace std;
+//建立关卡类
+class Start
+{
+public:
+	IMAGE img;
+	int x1, x2, y1, y2;//开始框的左上方和右下方的坐标
+	string location;
+	bool flag = false;
+	Start(int x1, int y1, int x2, int y2, string location) :x1(x1), x2(x2), y1(y1), y2(y2),location(location){}
+};
+
+//class pass
+//{
+//private:
+//	IMAGE img;
+//};
+
+
+//鼠标移动到某个位置时，通过颜色变化来指示所选位置
+//x1<x2,y1<y2
+bool RectangleInstruct(int x, int y,int x1,int y1,int x2,int y2,bool flag)
+{
+	if (x<x2 && x>x1 && y>y1 && y<y2&&flag==false)
+	{
+		cout << "flag: " << flag;
+		setlinestyle(PS_SOLID, 5);
+		setlinecolor(WHITE);
+		rectangle(x1, y1, x2, y2);
+		return true;
+	}
+	return false;
+}
+
+//导入第n关的图片
+void enter(IMAGE& img, LPCTSTR location)
+{
+	//cleardevice();
+	loadimage(&img, location);
+	putimage(0, 0, &img);
+}
+
+
 //植物类型种类
 enum { wandou, xiangrikui, jianguo, plant_count };
 IMAGE plant[plant_count];
-void begin()			//背景图片的载入
+void begin()
 {
 	IMAGE col;
 	loadimage(&col, L"sucai2/column.png");
@@ -83,6 +125,38 @@ private:
 
 int main()
 {
+	initgraph(900, 600, EX_SHOWCONSOLE);
+	Start start(279,321,425,395,"C:\\Users\\20443\\Desktop\\程序设计大作业\\图片素材\\Logo.jpg");
+
+
+	//获取鼠标信息
+	ExMessage msg;
+	while (true)
+	{
+		
+		enter(start.img, start.location.c_str());
+		if (peekmessage(&msg, EX_MOUSE))
+		{
+			int x = msg.x;
+			int y = msg.y;
+			if (RectangleInstruct(x, y, start.x1, start.y1, start.x2, start.y2,start.flag)&&msg.message==WM_LBUTTONDOWN)
+			{
+				start.flag = true;
+				MessageBox(NULL, "通关", "恭喜通关", MB_OK);
+				break;
+			}
+			else if (!RectangleInstruct(x, y, start.x1, start.y1, start.x2, start.y2, start.flag) && start.flag)
+			{
+				start.flag = false;
+				cleardevice();
+				enter(start.img, start.location.c_str());
+			}
+		}
+	}
+
+	//开始游戏矩形框的选择
+	getchar();
+	return 0;
 	IMAGE img;
 	IMAGE col;
 	initgraph(900, 600, 1);
