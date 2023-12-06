582884c05065137d1757e7769f16938539037a7f
heiferleaf <150570914+heiferleaf@users.noreply.github.com>
2023-12-06 17:33:36 +0800

    Update pvz.cpp

--- pvz.cpp
+++ pvz.cpp
@@ -4,6 +4,13 @@
 #include<vector>
 #include"tools.hpp"
 using namespace std;
+
+int curx, cury;//当前选中的植物，在移动过程中的位置
+int curZhiWu = 0;	//0:没有选中，1：选择了一种植物
+//用户点击,实现植物放置和拖拽
+bool finish = 0;
+bool dragging = 0;
+
 //建立开始界面
 class Start
 {
@@ -33,19 +40,38 @@ bool RectangleInstruct(int x, int y, int x1, int y1, int x2, int y2, bool flag)
 //所有的植物种类
 enum floristics{wandou,xaingrikui,jianguo};
 
+//设定grass类，表示草坪上的特定位置，并用occupied表示是否已经被放置
+class Grass {
+public:
+	int x, y;
+	bool occupied;
+
+	Grass(int x, int y) : x(x), y(y), occupied(false) {}
+};
+
 //建立关卡类
 class pass
 {
 public:
 	IMAGE img;
 	string location;
+	//当前关卡图片的大小
 	int sizex, sizey;
+	//当前关卡草坪的行列数
+	int line, row;
 	void movescene(IMAGE img, LPCTSTR locaiton, int sizex, int sizey);
 	void show_colume();
-	pass(string location, int sizex, int sizey,vector<int> plants):location(location),sizex(sizex),sizey(sizey),plants(plants){}
+	pass(string location, int sizex, int sizey, vector<int> plants, int line, int row);
 	void show_plants();
 	//本关植物编号的数组
 	vector<int>plants;
+	//表示本关的草坪
+	vector<Grass>grasses;
+	//判断第几个草坪，并且草坪上有无放置植物
+	int getGrassIndex(int x, int y);
+	void placePlant(int index);
+	
+
 };
 
 
@@ -88,6 +114,7 @@ class jian_guo :public plant
 
 };
 
+
 //导入第n关的图片
 void enter(IMAGE& img, LPCTSTR location)
 {
@@ -95,7 +122,22 @@ void enter(IMAGE& img, LPCTSTR location)
 	loadimage(&img, location,900,600);
 	putimage(0, 0, &img);
 }
-
+//参数待改
+pass::pass(string location, int sizex, int sizey, vector<int> plants,int line,int row) 
+:location(location), sizex(sizex), sizey(sizey), plants(plants),line(line),row(row)
+{
+	// 初始化草坪上的位置
+	for (int i = 0; i < line; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			//应该用变量存储草坪的起始位置与间隔大小
+			int x = 250 + j * 80;
+			int y = 177 + i * 100;
+			grasses.push_back(Grass(x, y));
+		}
+	}
+}
 
 void pass::movescene(IMAGE img, LPCTSTR location, int sizex, int sizey)
 {
@@ -138,7 +180,41 @@ void pass::show_plants()
 		putimage(182 + 56 * i , 6, &plant[i]);
 	}
 }
+//参数待改
+int pass::getGrassIndex(int x, int y) 
+{
+	for (int i = 0; i < grasses.size(); i++)
+	{
+		//应该用变量存储草坪的起始位置与间隔大小
+		if (x >= grasses[i].x && x <= grasses[i].x + 80 && y >= grasses[i].y && y <= grasses[i].y + 100) 
+		{
+			return i;
+		}
+	}
+	return -1; // 表示未找到合适的位置
+}
 
+void pass::placePlant(int index)
+{
+	int grassIndex = getGrassIndex(curx, cury);
+	if (grassIndex != -1 && !grasses[grassIndex].occupied)
+	{
+		// 根据index创建相应的植物对象，将其放置在草坪上
+		switch (plants[index]) 
+		{
+		case wandou: 
+		{
+			// 创建豌豆对象，放置在对应草坪上
+			wan_dou* wandou = new wan_dou(index);
+			drawImg(grasses[grassIndex].x, grasses[grassIndex].y, &img_wandou[0]);
+			// 在草坪上的位置标记为已被占据
+			grasses[grassIndex].occupied = true;
+			break;
+		}
+				   
+		}
+	}
+}
 IMAGE background; // 用于保存背景的 IMAGE 对象
 bool backgroundSaved = false; // 是否已保存背景
 
@@ -152,12 +228,10 @@ void restoreBackground(int x, int y)
 	putimage(x, y, &background);
 }
 
-int curx, cury;//当前选中的植物，在移动过程中的位置
-int curZhiWu = 0;	//0:没有选中，1：选择了一种植物
-//用户点击,实现植物放置和拖拽
-bool finish = 0;
-bool dragging = 0;
-void userClick(int count)
+
+
+
+void userClick(int count,pass cur_pass)
 {
 	ExMessage msg;
 	static bool status = 0;
@@ -188,6 +262,9 @@ void userClick(int count)
 		}
 		else if (msg.message == WM_LBUTTONUP&&dragging==1) 
 		{
+			cleardevice();
+			restoreBackground(0,0);
+			cur_pass.placePlant(curZhiWu - 1);
 			finish = 1;
 			dragging = 0;
 		}
@@ -228,7 +305,7 @@ int main()
 	//第一关植物
 	vector<int> pass1_plants = { wandou,xaingrikui,jianguo };
 	//建立第一关
-	pass pass1("pass1.jpg", 1250, 600, pass1_plants);
+	pass pass1("pass1.jpg", 1250, 600, pass1_plants,3,7);
 	BeginBatchDraw();
 	enter(pass1.img, pass1.location.c_str());
 	//场景移动
@@ -264,15 +341,39 @@ int main()
 			// 如果不是在拖拽状态，就保存整个屏幕作为背景
 			saveBackground(0,0);
 		}
-		userClick(pass1.plants.size());
+		userClick(pass1.plants.size(),pass1);
 
+		//if (dragging) 
+		//{
+		//	// 检查是否在草坪上
+		//	int index = pass1.getGrassIndex(curx, cury);
+		//	if (index != -1 && !pass1.grasses[index].occupied) 
+		//	{
+		//		curx = pass1.grasses[index].x;
+		//		cury = pass1.grasses[index].y;
+		//	}
+
+		//	restoreBackground(0, 0);
+		//	switch (curZhiWu - 1)
+		//	{
+		//	case(wandou):
+		//		drawImg(curx, cury, &img_wandou[0]);
+		//	}
+		//}
+
+		//// 在放置结束后更新草坪上对应位置的占据状态：
+		//if (finish) 
+		//{
+		//	int index = pass1.getGrassIndex(curx, cury);
+		//	if (index != -1 && !pass1.grasses[index].occupied)
+		//	{
+		//		pass1.grasses[index].occupied = true;
+		//	}
+
+		//	backgroundSaved = false;
+		//	finish = false;
+		//}
 		// 如果放置结束，清除背景保存标志
-		if (finish)
-		{
-			backgroundSaved = false;
-			finish = false;
-		}
-
 		FlushBatchDraw();
 	}
 	system("pause");
